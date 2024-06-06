bfda436315513ad6642015af5130b41d279dfe3d
Nabil_Bedjih <nabil.bedjih@gmail.com>
2024-06-07 00:20:33 +0200

    td 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LdapListComponent} from "./ldap-management/ldap-list/ldap-list.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {LdapDetailsComponent} from "./ldap-management/ldap-details/ldap-details.component";
+import {LdapAddComponent} from "./ldap-management/ldap-add/ldap-add.component";
+import {LdapEditComponent} from "./ldap-management/ldap-edit/ldap-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: PageNotFoundComponent }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
